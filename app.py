002254b8c8d2fd472df5e510c06a03f507a7f037
HardToPronounce <petcudavid2005@gmail.com>
2026-01-06 20:43:21 +0200

    done2

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from database import db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:copac123@localhost/Spital'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db.init_app(app)
 
 from models import Pacient, Medicament, Reteta
